1e91307c785929c39dffffc9eb664c7273ac9f63
Zaw Ye Htet Naing <zawye.official20@gmail.com>
2024-02-14 20:28:10 +0900

    initial changes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('Yo MaMa!')
 })
+
 app.listen(process.env.PORT || 3000)
